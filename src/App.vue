dcda21b0ce943b0de0a2d1d755c9f92ca795db51
Yaroslav <makhinko180601@gmail.com>
2023-12-24 07:58:55 +0100

    Add vue project Lesson_22

---
+++ src/App.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col">
+        <h2>Characteristics: ({{ totalPoints }}/20)</h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <ul class="list-group list-group-flush">
+          <character-setting v-for="character in characteristics" :key="character.id" :character="character"
+                             :total-points="totalPoints" @increment="incrementCount" @decrement="decrementCount"
+          />
+        </ul>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <pre>{{ characteristics }}</pre>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CharacterSetting from './CharacterSetting.vue'
+
+export default {
+  components: {
+    'character-setting': CharacterSetting,
+  },
+  data() {
+    return {
+      characteristics: [
+        {
+          id: 1,
+          title: 'Health',
+          count: 0,
+        },
+        {
+          id: 2,
+          title: 'Strength',
+          count: 0,
+        },
+        {
+          id: 3,
+          title: 'Intelligence',
+          count: 0,
+        },
+        {
+          id: 4,
+          title: 'Endurance',
+          count: 0,
+        },
+        {
+          id: 5,
+          title: 'Mana',
+          count: 0,
+        },
+        {
+          id: 6,
+          title: 'Armor',
+          count: 0,
+        },
+        {
+          id: 7,
+          title: 'Speed',
+          count: 0,
+        },
+      ],
+      totalPoints: 20,
+    };
+  },
+  methods: {
+    incrementCount(character) {
+      if (this.totalPoints > 0) {
+        character.count += 1
+        this.totalPoints -= 1;
+      }
+    },
+    decrementCount(character) {
+      if (character.count > 0) {
+        character.count -= 1
+        this.totalPoints += 1;
+      }
+    },
+  },
+};
+</script>
---
+++ src/CharacterSetting.vue
@@ -0,0 +1,37 @@
+<template>
+  <li class="list-group-item">
+    <div class="d-inline-block me-3">{{ character.title }}</div>
+    <div class="btn-group btn-group-sm" role="group" aria-label="Basic example">
+      <button type="button" class="btn btn-outline-primary" @click="decrement" :disabled="character.count === 0">-</button>
+      <button type="button" class="btn btn-primary" :disabled="true">{{ character.count }}</button>
+      <button type="button" class="btn btn-outline-primary" @click="increment" :disabled="totalPoints === 0">+</button>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    character: {
+      type: Object,
+      required: true,
+    },
+    totalPoints: {
+      type: Number,
+      required: true,
+    },
+  },
+  methods: {
+    increment() {
+      if (this.totalPoints > 0) {
+        this.$emit('increment', this.character);
+      }
+    },
+    decrement() {
+      if (this.character.count > 0) {
+        this.$emit('decrement', this.character);
+      }
+    },
+  },
+}
+</script>
